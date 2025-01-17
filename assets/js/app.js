46acb2f3794a81ba46a369eec6c68331b4e8888b
ochandoca <ochandoca@esat-alumni.com>
2025-01-17 12:00:14 +0000

    Add content in shelley manor project and code sample

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,8 +13,8 @@
 */
 
 // smooth scroll
-$(document).ready(function(){
-    $(".navbar .nav-link").on('click', function(event) {
+$(document).ready(function () {
+    $(".navbar .nav-link").on('click', function (event) {
 
         if (this.hash !== "") {
 
@@ -24,15 +24,15 @@ $(document).ready(function(){
 
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 700, function(){
+            }, 700, function () {
                 window.location.hash = hash;
             });
-        } 
+        }
     });
 });
 
 // protfolio filters
-$(window).on("load", function() {
+$(window).on("load", function () {
     var t = $(".portfolio-container");
     t.isotope({
         filter: ".new",
@@ -41,7 +41,7 @@ $(window).on("load", function() {
             easing: "linear",
             queue: !1
         }
-    }), $(".filters a").click(function() {
+    }), $(".filters a").click(function () {
         $(".filters .active").removeClass("active"), $(this).addClass("active");
         var i = $(this).attr("data-filter");
         return t.isotope({
